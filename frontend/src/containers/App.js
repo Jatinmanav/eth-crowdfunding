bb9915748795fc300be002fc0391048cef571c11
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-04-05 15:04:22 +0530

    added signin and signup pages

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContextProvider } from "../contexts/AuthContext";
+import Signin from "../components/Signin";
+import Signup from "../components/Signup";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <AuthContextProvider>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/signin">
+              <Signin />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthContextProvider>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
