17ca92fc53ca55f277e5c62a727f2aa0bf0c2e4a
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-05-06 22:27:52 +0530

    added sidebar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,9 @@
 import React, { useContext } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -8,8 +12,30 @@ import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+import ViewModuleRoundedIcon from "@material-ui/icons/ViewModuleRounded";
+import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import colorVariables from "../utils/ColorVariables";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: colorVariables.fontColor1,
+    },
+    secondary: {
+      main: "#ddd",
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,12 +51,27 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  list: {
+    width: 250,
+  },
+  drawer: {
+    backgroundColor: colorVariables.backgroundColor1,
+  },
+  listItem: {
+    textDecoration: "none",
+    color: colorVariables.fontColor1,
+    textAlign: "center",
+  },
+  fullList: {
+    width: "auto",
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
   const { auth, setLoggedOut } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [state, setState] = React.useState(false);
   const open = Boolean(anchorEl);
 
   const handleMenu = (event) => {
@@ -46,54 +87,131 @@ const Header = () => {
     setAnchorEl(null);
   };
 
-  return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Eth-Crowdfunding
-          </Typography>
-          {auth.isAuthenticated && (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleLogout}>Logout</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState(open);
+  };
+
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <Link to="/" className={classes.listItem}>
+          <ListItem button key="home">
+            <ListItemIcon>
+              <HomeRoundedIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+        </Link>
+        <Link to="/browse" className={classes.listItem}>
+          <ListItem button key="browse">
+            <ListItemIcon>
+              <ViewModuleRoundedIcon color="secondary" />
+            </ListItemIcon>
+            <ListItemText primary="Browse" />
+          </ListItem>
+        </Link>
+      </List>
+      {auth.isAuthenticated ? <React.Fragment /> : <Divider />}
+      {auth.isAuthenticated ? (
+        <React.Fragment />
+      ) : (
+        <List>
+          <Link to="/signup" className={classes.listItem}>
+            <ListItem button key="signup">
+              <ListItemIcon>
+                <AccountCircleRoundedIcon color="secondary" />
+              </ListItemIcon>
+              <ListItemText primary="Signup" />
+            </ListItem>
+          </Link>
+          <Link to="/signin" className={classes.listItem}>
+            <ListItem button key="signup">
+              <ListItemIcon>
+                <AccountCircleRoundedIcon color="secondary" />
+              </ListItemIcon>
+              <ListItemText primary="Signin" />
+            </ListItem>
+          </Link>
+        </List>
+      )}
     </div>
+  );
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <div className={classes.root}>
+          <AppBar className={classes.appBar} position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Eth-Crowdfunding
+              </Typography>
+              {auth.isAuthenticated && (
+                <div>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div>
+          <Drawer
+            classes={{ paper: classes.drawer }}
+            anchor="left"
+            open={state}
+            onClose={toggleDrawer(false)}
+          >
+            {list()}
+          </Drawer>
+        </div>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,26 +1,40 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthContextProvider } from "../contexts/AuthContext";
 import Signin from "../components/Signin";
 import Signup from "../components/Signup";
 import Header from "../components/Header";
+import colorVariables from "../utils/ColorVariables";
+
+const useStyles = makeStyles(() => ({
+  "@global": {
+    body: {
+      backgroundColor: colorVariables.backgroundColor1,
+    },
+  },
+}));
 
 export default function App() {
+  //eslint-disable-next-line
+  const classes = useStyles();
+
   return (
     <AuthContextProvider>
-      <Header />
       <Router>
+        <Header />
         <div>
           <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
             <Route path="/signin">
               <Signin />
             </Route>
             <Route path="/signup">
               <Signup />
             </Route>
-            <Route path="/">
-              <Home />
-            </Route>
+            <Route path="/" render={() => <h1>Error 404: Page not found</h1>} />
           </Switch>
         </div>
       </Router>
