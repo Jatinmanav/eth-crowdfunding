f7e52a507cbc6842b6582947914a6c4ec08bf1e5
Jatinmanav <46203753+Jatinmanav@users.noreply.github.com>
2020-06-09 22:25:25 +0530

    optimized routes

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthContextProvider } from "../contexts/AuthContext";
+import { AuthContext } from "../contexts/AuthContext";
 import Signin from "../components/Signin";
 import Signup from "../components/Signup";
 import Header from "../components/Header";
@@ -10,34 +10,33 @@ import Campaign from "../components/Campaign";
 import Newcampaign from "../components/Newcampaign";
 
 export default function App() {
+  const { auth } = useContext(AuthContext);
   return (
-    <AuthContextProvider>
-      <Router>
-        <Header />
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/signin">
-              <Signin />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-            <Route path="/explore">
-              <Explore />
-            </Route>
-            <Route path="/create-campaign">
-              <Newcampaign />
-            </Route>
-            <Route path="/campaigns/">
-              <Campaign />
-            </Route>
-            <Route path="/" render={() => <h1>Error 404: Page not found</h1>} />
-          </Switch>
-        </div>
-      </Router>
-    </AuthContextProvider>
+    <Router>
+      <Header />
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signin">
+            {auth.isAuthenticated ? <Home /> : <Signin />}
+          </Route>
+          <Route path="/signup">
+            {auth.isAuthenticated ? <Home /> : <Signup />}
+          </Route>
+          <Route path="/explore">
+            <Explore />
+          </Route>
+          <Route path="/create-campaign">
+            {auth.isAuthenticated ? <Newcampaign /> : <Signin />}
+          </Route>
+          <Route path="/campaigns/">
+            <Campaign />
+          </Route>
+          <Route path="/" render={() => <h1>Error 404: Page not found</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { AuthContextProvider } from "./contexts/AuthContext";
 import App from "./containers/App";
 
 ReactDOM.render(
   <React.Fragment>
-    <CssBaseline />
-    <App />
+    <AuthContextProvider>
+      <CssBaseline />
+      <App />
+    </AuthContextProvider>
   </React.Fragment>,
   document.getElementById("root")
 );
